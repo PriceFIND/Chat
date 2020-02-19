b58aca89160288453742596e3cb6cb1681d8ab8f
Jenny <61031870+tosh20@users.noreply.github.com>
2020-02-18 17:28:49 -0700

    Add files via upload

---
+++ oneChat.php
@@ -0,0 +1,50 @@
+<?php
+
+$mysqli=new mysqli("localhost","Buffalo","Toshy200","chatty");
+
+$roomName = $_POST["room"]
+
+$result=$mysqli->query("
+SELECT chat_messages.message, user_names.name AS 'user name', 
+	chat_rooms.name_of_room AS 'room'
+FROM chat_messages
+		INNER JOIN user_names ON
+		chat_messages.sender = user_names.ID
+		INNER JOIN chat_rooms ON
+		chat_messages.room = '$roomName'
+");
+
+$result_count = $mysqli->field_count;
+
+
+$html="
+<html>
+	<head>
+	</head>
+	<body>
+	<table>
+		<tr>
+			<td><b>ID</b></td>
+			<td><b>User</b></td>
+			<td><b>Room</b></td>
+		</tr>
+";
+
+while($row=$result->fetch_array()){
+	$html.="<tr>";
+	for ($i=0; $i<$result_count; $i++){
+		$html.="<td>".$row[$i]."</td>";
+
+}
+$html.="</tr>";
+}
+
+$html.="
+</table>
+</body>
+</html>";
+
+echo $html;
+
+
+?>
